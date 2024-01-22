b21c74d9021d1d52a74f863746294624d44a9e3b
bruno-PC\bruno <bruno.doue@laposte.net>
2024-01-22 23:14:22 +0100

    fix css cart

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -16,10 +16,10 @@ class OrdersController extends AbstractController
 
         $cart = $session->get('cart', []);//Get the cart if exist, otherwise retrieve an empty table
         // dd($cart);
-if($cart === []){
-    $this->addFlash('notice', 'votre panier est vide');
-    $this->redirectToRoute('app_home');
-})
+        if($cart === []){
+            $this->addFlash('notice', 'votre panier est vide');
+            $this->redirectToRoute('app_home');
+        };
 
         return $this->render('orders/index.html.twig', [
             'controller_name' => 'OrdersController',
